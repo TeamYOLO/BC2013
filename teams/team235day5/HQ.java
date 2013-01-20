2456575dfb939d00354b01c50bfac0a132835b9e
Ryan DuToit <Ryan.DuToit@drake.edu>
2013-01-20 11:25:41 -0600

    added day5 bot

---
+++ teams/team235day5/HQ.java
@@ -0,0 +1,287 @@
+package team235day5;
+
+import battlecode.common.*;
+
+public class HQ {
+	private static int allInRound = -100;
+
+	private static int rallyRadius = 33;
+
+	private static int massAmount = 25;
+
+	private static RobotController rc;
+	private static double minPowerThreshold = 100; //TODO-findthisvalue
+	private static double minRoundThreshold = 100; //TODO-findthisvalue
+	private static int gencount = 0;
+	private static int soldiercount = 0;
+	private static int othercount = 0;
+
+	private static int optimalBuildings = 0;
+	private static int farAwayButSafeBuildings = 0;
+
+	private static int powerThreshold =500;
+
+	private static boolean expandPhase = true;
+	
+	private static Robot[] alliedRobots;
+	private static Robot[] enemyRobots;
+	
+	private static int rushDistance;
+	
+	public static void hqCode(RobotController myRC) throws GameActionException
+	{
+		rc = myRC;
+		Direction defaultSpawnDir = rc.getLocation().directionTo(rc.senseEnemyHQLocation());
+		evaluateMap();
+		while(true) 
+		{
+			rc.broadcast(Constants.attackChannel, 0);
+			alliedRobots = rc.senseNearbyGameObjects(Robot.class, new MapLocation(0,0), 1000000, rc.getTeam());
+			enemyRobots = rc.senseNearbyGameObjects(Robot.class, new MapLocation(0,0), 1000000, rc.getTeam().opponent());
+
+			
+			if(expandOrRally())
+			{
+				rc.broadcast(Constants.commandChannel, Constants.commandExpand);
+			}
+			else
+			{
+				MapLocation rally = findRallyPoint();
+				rc.broadcast(Constants.rallyXChannel, rally.x);
+				rc.broadcast(Constants.rallyYChannel, rally.y);
+				rc.broadcast(Constants.commandChannel,Constants.commandRally);
+			}
+
+			if (rc.isActive()) 
+			{
+				int readIn = rc.readBroadcast(Constants.campChannel);
+
+				if(!doWeNeedGenerator())
+				{
+					rc.broadcast(Constants.campChannel, Constants.campSupplier);
+
+					// Spawn a soldier
+					Team defaultScan = rc.senseMine(rc.getLocation().add(defaultSpawnDir));
+					if (rc.canMove(defaultSpawnDir) && (defaultScan == null || defaultScan == rc.getTeam()))
+					{
+						rc.spawn(defaultSpawnDir);
+					}
+					else
+					{
+						for(Direction d : Direction.values()) // TODO: optimize secondary direction finding
+						{
+							if(d != Direction.OMNI && d != Direction.NONE)
+							{
+								Team scan = rc.senseMine(rc.getLocation().add(d));
+								if(rc.canMove(d) && (scan == null || scan == rc.getTeam()))
+								{
+									rc.spawn(d);
+									break;
+								}
+							}
+						}
+						if(rc.isActive())
+						{
+							// if there are no valid spawn directions
+							rc.researchUpgrade(Upgrade.NUKE);
+						}
+					}
+				}
+				else // we do need a generator
+				{
+					if(readIn == Constants.campSupplier || (readIn != Constants.campGenInProduction && readIn != Constants.campGen))
+					{
+						rc.broadcast(Constants.campChannel, Constants.campGen);
+					}
+					if(!rc.hasUpgrade(Upgrade.FUSION))
+					{
+						rc.researchUpgrade(Upgrade.FUSION);
+					}
+					else if(!rc.hasUpgrade(Upgrade.DEFUSION))
+					{
+						rc.researchUpgrade(Upgrade.DEFUSION);
+					}
+					else
+					{
+						rc.researchUpgrade(Upgrade.NUKE);
+					}
+				}
+			}
+
+			if(Clock.getRoundNum() > 200)
+			{
+				if(rc.senseEnemyNukeHalfDone())
+				{
+					rc.broadcast(Constants.commandChannel, Constants.commandEnemyNukeHalfDone);
+				}
+			}
+
+			shallWeAllIn();
+
+			rc.yield();
+		}
+	}
+
+	private static void shallWeAllIn() throws GameActionException
+	{
+		int massedRobos = 0;
+
+		Robot[] robos = rc.senseNearbyGameObjects(Robot.class, findRallyPoint(), rallyRadius, rc.getTeam());
+		for(Robot r : robos)
+		{
+			if(rc.senseRobotInfo(r).type == RobotType.SOLDIER)
+			{
+				++massedRobos;
+			}
+		}
+
+		if(massedRobos > .8*(40 + (10 * gencount) - (1 * othercount))) // if we should all in...
+		{
+			rc.broadcast(Constants.attackChannel, Constants.attackAllIn);
+			allInRound = Clock.getRoundNum();
+		}
+	}
+
+	private static MapLocation findRallyPoint() throws GameActionException
+	{
+		
+		MapLocation closestEnemy = null;
+		closestEnemy = Util.findClosestRobot(rc, enemyRobots);
+		if(closestEnemy != null && closestEnemy.distanceSquaredTo(rc.getLocation()) < rushDistance*.2) {
+			return closestEnemy;
+		}
+		
+		
+		
+		MapLocation enemyLoc = rc.senseEnemyHQLocation();
+		MapLocation ourLoc = rc.senseHQLocation();
+		int x = (enemyLoc.x + 2 * ourLoc.x) / 3;
+		int y = (enemyLoc.y + 2 * ourLoc.y) / 3;
+		return new MapLocation(x,y);
+	}
+
+	public static boolean doWeNeedGenerator() throws GameActionException
+	{
+		if(rc.readBroadcast(Constants.campChannel) == Constants.campGenInProduction) return false;
+		if(Clock.getRoundNum() - allInRound < 80) return false;
+		if(rc.readBroadcast(Constants.commandChannel) == Constants.commandRally && rc.getTeamPower()>powerThreshold) return false;
+
+		if(rc.getTeamPower() < minPowerThreshold && Clock.getRoundNum() > minRoundThreshold)
+		{
+			return true;
+		}
+
+		gencount = 0;
+		soldiercount = 0;
+		othercount = 0;
+
+		Robot[] robos = rc.senseNearbyGameObjects(Robot.class, new MapLocation(0,0), 1000000, rc.getTeam());
+		for(Robot r : robos)
+		{
+			RobotType rt = rc.senseRobotInfo(r).type;
+			if(rt == RobotType.GENERATOR) ++gencount;
+			else if(rt == RobotType.SOLDIER) ++soldiercount;
+			else ++othercount;
+		}
+
+		double decay = .8;
+		if(rc.hasUpgrade(Upgrade.FUSION))
+		{
+			decay = .99;
+		}
+
+		if((40 + (10 * gencount) - (1.6 * soldiercount) - (1 * othercount)) * decay < 1)
+		{
+			return true;
+		}
+		return false;
+	}
+	public static void evaluateMap() throws GameActionException {
+		MapLocation neutralCamps[] = rc.senseEncampmentSquares(rc.getLocation(), 1000000, Team.NEUTRAL);
+		MapLocation me = rc.getLocation();
+		MapLocation them = rc.senseEnemyHQLocation();
+		rushDistance = them.distanceSquaredTo(me);
+
+		for (MapLocation loc : neutralCamps) {
+			double toUs=loc.distanceSquaredTo(me);
+			double toThem = loc.distanceSquaredTo(them);			
+			if(toUs/toThem < .81)
+			{
+				if(rushDistance > toUs)
+					optimalBuildings++;
+				else farAwayButSafeBuildings++;
+			}
+		}
+	}
+
+	public static void singleExpand() throws GameActionException
+	{
+		int numCamps = rc.senseAlliedEncampmentSquares().length;
+		if(numCamps >= optimalBuildings + farAwayButSafeBuildings)
+			return;
+
+		if(numCamps < optimalBuildings)
+		{
+
+		}
+
+		if(false) { //figure out condition
+			MapLocation expansion = null;
+			expansion = findClosestEmptyCamp();
+			rc.broadcast(Constants.commandChannel, Constants.commandBuildIndividual);
+			rc.broadcast(Constants.singleExpandXChannel, expansion.x);
+			rc.broadcast(Constants.singleExpandYChannel, expansion.y);
+		}
+	}
+
+	public static boolean expandOrRally() throws GameActionException //true for expand
+	{
+		// rush distance
+		//double rushDistance = rc.senseHQLocation().distanceSquaredTo(rc.senseEnemyHQLocation());
+		// number of encampments
+
+		if(!expandPhase)
+		{ 
+			singleExpand();
+			return false;
+		}
+
+		int numAlliedCamps = rc.senseAlliedEncampmentSquares().length;
+		if(numAlliedCamps > 13)
+		{
+			rc.broadcast(Constants.commandChannel, Constants.commandRally);
+			expandPhase = false;
+			return false;
+		}
+
+		if(numAlliedCamps>=optimalBuildings)
+		{
+			rc.broadcast(Constants.commandChannel, Constants.commandRally);
+			expandPhase = false;
+			return false;
+		} 
+
+		rc.broadcast(Constants.commandChannel, Constants.commandExpand);
+		return true;
+	}
+
+	private static MapLocation findClosestEmptyCamp() throws GameActionException
+	{
+		MapLocation[] locArray = rc.senseEncampmentSquares(rc.getLocation(), 1000000, Team.NEUTRAL);
+		int closestDist = 1000000;
+		MapLocation me = rc.getLocation();
+		MapLocation closestLocation = null;
+		for (int i = 0; i < locArray.length; i++)
+		{
+			MapLocation aLocation = locArray[i];
+			int dist = aLocation.distanceSquaredTo(me);
+			if (dist < closestDist && rc.senseNearbyGameObjects(Robot.class, aLocation, 0, rc.getTeam()).length < 1)
+			{
+				closestDist = dist;
+				closestLocation = aLocation;
+			}
+		}
+		return closestLocation;
+	}
+
+}
---
+++ teams/team235day5/Soldier.java
@@ -0,0 +1,335 @@
+package team235day5; // test
+
+import battlecode.common.*;
+
+public class Soldier
+{
+	private static int rallyRadius = 25;
+
+	private final static int localScanRange = 14;
+
+
+	private static boolean localscan = false;
+
+	private static RobotController rc;
+	private static MapLocation rallyPoint;
+
+	public static void soldierCode(RobotController myRC) throws GameActionException
+	{
+		rc = myRC;
+		rallyPoint = findRallyPoint();
+		while(true)
+		{
+			try
+			{
+				// returns all enemy robots within our sight range (aka that we can actually fight right now or very soon)
+				Robot[] closeEnemyRobots = rc.senseNearbyGameObjects(Robot.class, rc.getLocation(), 14, rc.getTeam().opponent());
+				
+				// returns all enemy robots reasonably close to us (aka that we should move toward and potentially assist our allies in fighting)
+				Robot[] enemyRobots = rc.senseNearbyGameObjects(Robot.class,63,rc.getTeam().opponent());
+				
+				if(closeEnemyRobots.length == 0 && enemyRobots.length == 0) // no enemies are nearby
+				{
+					int command = HQCommand();
+					switch (command) {
+					case Constants.commandExpand:
+						expand();
+						break;
+					case Constants.commandRally:
+						rally();
+						break;
+					case Constants.commandEnemyNukeHalfDone:
+						rally();
+					case Constants.commandBuildIndividual: //TODO- implement this
+						break;
+					}
+				}
+				else
+				{
+
+					// enemy spotted
+					localscan = false;
+					MapLocation closestEnemy = findClosestRobot(enemyRobots);
+					goToLocation(closestEnemy);
+				}
+			}
+			catch (Exception e)
+			{
+				System.out.println("Soldier Exception");
+				e.printStackTrace();
+			}
+			rc.yield();
+		}
+	}
+	
+
+	private static MapLocation findRallyPoint() throws GameActionException
+	{
+		return new MapLocation(rc.readBroadcast(Constants.rallyXChannel), rc.readBroadcast(Constants.rallyYChannel));
+	}
+
+	private static int getNumberOfAlliedRobosAfterMe() throws GameActionException
+	{
+		int retval = 0;
+		int myID = rc.getRobot().getID();
+
+		Robot[] robos = rc.senseNearbyGameObjects(Robot.class, new MapLocation(0,0), 1000000, rc.getTeam());
+
+		for(Robot r : robos)
+		{
+			if(r.getID() > myID)
+			{
+				++retval;
+			}
+		}
+
+		return retval;
+	}
+
+	private static void expand() throws GameActionException
+	{
+		//rallyPoint = findClosestLocation(rc.senseAllEncampmentSquares());
+		if(!localscan) {
+			rallyPoint = findClosestEmptyCamp();
+		}
+		if(rc.getLocation().distanceSquaredTo(rallyPoint) < 1) // if we are at the location of the rally point
+		{
+			if(!localscan) {
+				rallyPoint = findFurthestLocalCamp();
+				localscan=true;
+			}
+		}
+		if(rc.getLocation().distanceSquaredTo(rallyPoint) < 1) // if we are at the location of the rally point
+		{
+
+
+			if(rc.isActive()) // if we are allowed to capture
+			{
+				if(rc.senseCaptureCost() + 1.8 * getNumberOfAlliedRobosAfterMe() < rc.getTeamPower()) // if we have enough power to capture
+				{
+					int readIn = rc.readBroadcast(Constants.campChannel);
+					if(readIn == Constants.campGen)
+					{
+						rc.broadcast(Constants.campChannel, Constants.campGenInProduction);
+						rc.captureEncampment(RobotType.GENERATOR);
+					}
+					else if(readIn == Constants.campGenInProduction)
+					{
+						rc.captureEncampment(RobotType.SUPPLIER);
+					}
+					else if(readIn == Constants.campSupplier)
+					{ 
+						rc.captureEncampment(RobotType.SUPPLIER);
+					}
+					else // TODO: transmissions may be being scrambled, for now just make supplier
+					{
+						rc.captureEncampment(RobotType.SUPPLIER);
+					}
+				}
+			}
+		}
+		else if(rc.senseNearbyGameObjects(Robot.class, rallyPoint, 0, rc.getTeam()).length > 0) // if there is an allied robot on our rally point
+		{
+			rallyPoint = findClosestEmptyCamp();
+			if(rallyPoint == null)
+			{
+				rallyPoint = findRallyPoint();
+			}
+			goToLocation(rallyPoint);
+		}
+		else
+		{
+			goToLocation(rallyPoint);
+		}
+	}
+
+	private static void rally() throws GameActionException
+	{
+		rallyPoint = findRallyPoint();
+		// if we are fairly close to the rally point and we have the necessary soldier counts to make up a wave, gogogogogo
+		if(rc.getLocation().distanceSquaredTo(rallyPoint) < rallyRadius)
+		{
+			if(rc.readBroadcast(Constants.attackChannel) == Constants.attackAllIn)
+			{
+				while(true)
+				{
+					Robot[] closeEnemyRobots = rc.senseNearbyGameObjects(Robot.class, rc.getLocation(), 14, rc.getTeam().opponent());
+					if(closeEnemyRobots.length == 0) {
+						goToLocation(rc.senseEnemyHQLocation());
+					}
+					else {
+						goToLocation(Util.findClosestRobot(rc, closeEnemyRobots));
+					}
+					rc.yield();
+				}
+			}
+			else
+			{
+				goToLocation(rallyPoint);
+			}
+		}
+		else 
+		{
+			goToLocation(rallyPoint);
+		}
+	}
+
+	private static MapLocation findClosestRobot(Robot[] enemyRobots) throws GameActionException
+	{
+		int closestDist = 1000000;
+		MapLocation closestEnemy = null;
+		for (int i = 0; i < enemyRobots.length; i++)
+		{
+			Robot arobot = enemyRobots[i];
+			RobotInfo arobotInfo = rc.senseRobotInfo(arobot);
+			int dist = arobotInfo.location.distanceSquaredTo(rc.getLocation());
+			if (dist<closestDist){
+				closestDist = dist;
+				closestEnemy = arobotInfo.location;
+			}
+		}
+		return closestEnemy;
+	}
+
+	private static MapLocation findClosestLocation(MapLocation[] locArray) throws GameActionException {
+		int closestDist = 1000000;
+		MapLocation me = rc.getLocation();
+		MapLocation closestLocation=null;
+		for (int i=0; i<locArray.length; i++)
+		{
+			MapLocation aLocation = locArray[i];
+			int dist = aLocation.distanceSquaredTo(me);
+			if (dist < closestDist)
+			{
+				closestDist = dist;
+				closestLocation = aLocation;
+			}
+		}
+		return closestLocation;
+	}
+	private static MapLocation findFurthestLocalCamp() throws GameActionException {
+		MapLocation result = null;
+		MapLocation me = rc.getLocation();
+		MapLocation[] locArray = rc.senseEncampmentSquares(me, localScanRange, Team.NEUTRAL);
+		int furthestDist = -1;
+
+		for (int i = 0; i < locArray.length; i++)
+		{
+			MapLocation aLocation = locArray[i];
+			int dist = aLocation.distanceSquaredTo(me);
+			if (dist > furthestDist && rc.senseNearbyGameObjects(Robot.class, aLocation, 0, rc.getTeam()).length < 1)
+			{
+				furthestDist = dist;
+				result = aLocation;
+			}
+		}
+		return result;
+
+	}
+	private static MapLocation findClosestEmptyCamp() throws GameActionException
+	{
+		MapLocation[] locArray = rc.senseEncampmentSquares(rc.getLocation(), 1000000, Team.NEUTRAL);
+		int closestDist = 1000000;
+		MapLocation me = rc.getLocation();
+		MapLocation closestLocation = null;
+		for (int i = 0; i < locArray.length; i++)
+		{
+			MapLocation aLocation = locArray[i];
+			int dist = aLocation.distanceSquaredTo(me);
+			if (dist < closestDist && rc.senseNearbyGameObjects(Robot.class, aLocation, 0, rc.getTeam()).length < 1)
+			{
+				closestDist = dist;
+				closestLocation = aLocation;
+			}
+		}
+		return closestLocation;
+	}
+
+	private static void goToLocation(MapLocation whereToGo) throws GameActionException
+	{
+		MapLocation meee = rc.getLocation();
+		int dist = meee.distanceSquaredTo(whereToGo);
+		if (dist > 0 && rc.isActive())
+		{
+			Direction dir = meee.directionTo(whereToGo);
+			int[] directionOffsets = {0, 1, -1, 2, -2};
+			Direction lookingAtCurrently = null;
+			for (int d : directionOffsets){
+				lookingAtCurrently = Direction.values()[(dir.ordinal()+d+8)%8];
+				if(rc.canMove(lookingAtCurrently))
+				{
+					moveOrDefuse(lookingAtCurrently);
+					break;
+				}
+			}
+		}
+	}
+
+	private static void moveOrDefuse(Direction dir) throws GameActionException
+	{
+		MapLocation ahead = rc.getLocation().add(dir);
+		if(rc.senseMine(ahead) != null)
+		{
+			rc.defuseMine(ahead);
+		}
+		else
+		{
+			rc.move(dir);
+		}
+	}
+
+	public static int HQCommand() throws GameActionException // true for expand
+	{
+		return rc.readBroadcast(Constants.commandChannel);
+	}
+
+	public static void expandIndividual() throws GameActionException
+	{
+		MapLocation expandLocation = new MapLocation(rc.readBroadcast(Constants.singleExpandXChannel),rc.readBroadcast(Constants.singleExpandYChannel));
+		rc.broadcast(Constants.commandChannel, Constants.commandRally);
+		while(true) {
+			if(rc.getLocation().distanceSquaredTo(expandLocation) < 1) // if we are at the location of the rally point
+			{
+				if(rc.isActive()) // if we are allowed to capture
+				{
+					if(rc.senseCaptureCost() + 1.8 * getNumberOfAlliedRobosAfterMe() < rc.getTeamPower()) // if we have enough power to capture
+					{
+						int readIn = rc.readBroadcast(Constants.campChannel);
+						if(readIn == Constants.campGen)
+						{
+							rc.broadcast(Constants.campChannel, Constants.campGenInProduction);
+							rc.captureEncampment(RobotType.GENERATOR);
+						}
+						else if(readIn == Constants.campGenInProduction)
+						{
+							rc.captureEncampment(RobotType.SUPPLIER);
+						}
+						else if(readIn == Constants.campSupplier)
+						{ 
+							rc.captureEncampment(RobotType.SUPPLIER);
+						}
+						else // TODO: transmissions may be being scrambled, for now just make supplier
+						{
+							rc.captureEncampment(RobotType.SUPPLIER);
+						}
+						break;
+					}
+				}
+			}
+			else if(rc.senseNearbyGameObjects(Robot.class, expandLocation, 0, rc.getTeam()).length > 0) // if there is an allied robot on our rally point
+			{
+				expandLocation = findClosestEmptyCamp();
+				if(expandLocation == null)
+				{
+					expandLocation = findRallyPoint();
+				}
+				goToLocation(expandLocation);
+			}
+			else
+			{
+				goToLocation(expandLocation);
+			}
+			rc.yield();
+		}
+	}
+}
