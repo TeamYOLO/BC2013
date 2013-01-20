baee2d03ffff97dbe4ead066b232fbaffbb842b4
Ryan DuToit <Ryan.DuToit@drake.edu>
2013-01-20 11:17:31 -0600

    added crazyRallyPointBot

---
+++ teams/crazyRallyPointBot/RobotPlayer.java
@@ -0,0 +1,38 @@
+package crazyRallyPointBot;
+
+import battlecode.common.*;
+
+public class RobotPlayer
+{
+	private static RobotController rc;
+	
+	public static void run(RobotController myRC)
+	{
+		rc = myRC;
+		RobotType t = rc.getType();
+
+		try
+		{
+			if (t == RobotType.SOLDIER)
+			{
+				Soldier.soldierCode(rc);
+			}
+			else if(t == RobotType.HQ)
+			{
+				HQ.hqCode(rc);
+			}
+			else
+			{
+				while(true)
+				{
+					rc.yield();
+				}
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println("caught exception before it killed us:");
+			e.printStackTrace();
+		}
+	}
+}
